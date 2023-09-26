1ed644b7d366304d96276eccdd039851569d7853
amonnatt <momo88741374@gmail.com>
2023-09-26 15:52:33 +0700

    upload

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,20 +1,33 @@
-import React from 'react'
-import { useEffect, useRef, useState } from 'react';
+import React, { useState } from 'react'
+import { MdCloudUpload } from 'react-icons/md'
+//import { AiFillFileImage,MdDete } from 'react-icons/ai'
+
 import '../css/Home.css'
 function Home (){
+
     const [image, setImage] = useState(null)
-    const [fileName, setFileName] = useState("No selected file")
+    const [fileName, setFileName] =useState("No selected file")
     return (
      <main>
-        <form action='' 
+        <button action='' 
         className='upbox'
-        onClick={() => document.querySelector(".input-field").Click()}
+        onClick={() => document.querySelector(".input-field").Click}
         >
-            <input type='file' name='image/*'>
+            <input type='file' name='image/*' hidden 
+            onChange={({target:{files}}) => {
+                files[0] && setFileName(files[0].name)
+                if(files){
+                    setImage(URL.createObjectURL(files[0]))
+                }
+            }}
+            />
 
-            </input>
-            
-        </form>
+            {image ?
+            <img scr={image} width={60} height={60} alt={fileName} />
+            :
+            <MdCloudUpload color='#1475cf' size={60} />
+        }
+        </button>
      </main>   
     )
 
