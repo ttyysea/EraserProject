6e5a4e2275f9592d8fe736c40ed3eebe51cb47d9
amonnatt <momo88741374@gmail.com>
2023-09-26 11:33:29 +0700

    box upload

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,7 +1,22 @@
-const Home = () => {
-    return <div>
-        <i class='bx bxs-home'></i>,
-    </div>;
-};
+import React from 'react'
+import { useEffect, useRef, useState } from 'react';
+import '../css/Home.css'
+function Home (){
+    const [image, setImage] = useState(null)
+    const [fileName, setFileName] = useState("No selected file")
+    return (
+     <main>
+        <form action='' 
+        className='upbox'
+        onClick={() => document.querySelector(".input-field").Click()}
+        >
+            <input type='file' name='image/*'>
 
+            </input>
+            
+        </form>
+     </main>   
+    )
+
+}
 export default Home;
